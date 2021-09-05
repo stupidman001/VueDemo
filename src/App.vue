0ea37ef4fa9d3e86da9b6a8309667dd6ae43528f
githubjyj <2594131385@qq.com>
2021-09-05 17:50:57 +0800

    完成搜索框的功能

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,98 @@
 <template>
-  <div id="app">
-    
+  <div class="todo-container">
+    <div class="todo-wrap">
+      <MyHeader :receive="receive"/>
+      <MyList :todos="todos" />
+      <MyFooter />
+    </div>
   </div>
 </template>
 
 <script>
+import MyHeader from "@/components/MyHeader.vue";
+import MyFooter from "@/components/MyFooter.vue";
+import MyList from "@/components/MyList.vue";
 
 export default {
-  name: 'App',
-}
+  name: "App",
+  components: {
+    MyHeader,
+    MyFooter,
+    MyList,
+  },
+  data() {
+    // 数据存放在 App 中
+    return {
+      todos: [
+        {
+          id: "001",
+          title: "吃饭",
+          done: true,
+        },
+        {
+          id: "002",
+          title: "睡觉",
+          done: false,
+        },
+        {
+          id: "003",
+          title: "开车",
+          done: true,
+        },
+      ],
+    };
+  },
+  methods:{
+    receive(item){
+      this.todos.unshift(item)
+    }
+  }
+};
 </script>
+
+<style>
+/*base*/
+body {
+  background: #fff;
+}
+/* 按钮的样式 */
+.btn {
+  display: inline-block;
+  padding: 4px 12px;
+  margin-bottom: 0;
+  font-size: 14px;
+  line-height: 20px;
+  text-align: center;
+  vertical-align: middle;
+  cursor: pointer;
+  box-shadow: inset 0 1px 0 rgba(255, 255, 255, 0.2),
+    0 1px 2px rgba(0, 0, 0, 0.05);
+  border-radius: 4px;
+}
+
+.btn-danger {
+  color: #fff;
+  background-color: #da4f49;
+  border: 1px solid #bd362f;
+}
+
+.btn-danger:hover {
+  color: #fff;
+  background-color: #bd362f;
+}
+
+.btn:focus {
+  outline: none;
+}
+/* 按钮的样式完 */
+
+.todo-container {
+  width: 600px;
+  margin: 0 auto;
+}
+.todo-container .todo-wrap {
+  padding: 10px;
+  border: 1px solid #ddd;
+  border-radius: 5px;
+}
+</style>
---
+++ src/components/MyList.vue
@@ -0,0 +1,38 @@
+<template>
+  <ul class="todo-main">
+    <MyItem v-for="item in todos" :key='item.id' :todo="item" />
+  </ul>
+</template>
+
+<script>
+import MyItem from "@/components/MyItem.vue";
+export default {
+  name: "MyList",
+  props:['todos'],
+  components: {
+    MyItem,
+  },
+  created(){
+    console.log(this.todos)
+  }
+};
+</script>
+
+<style scoped>
+/*main*/
+.todo-main {
+  margin-left: 0px;
+  border: 1px solid #ddd;
+  border-radius: 2px;
+  padding: 0px;
+}
+
+.todo-empty {
+  height: 40px;
+  line-height: 40px;
+  border: 1px solid #ddd;
+  border-radius: 2px;
+  padding-left: 5px;
+  margin-top: 10px;
+}
+</style>
